556d54d8d89dfea334ae6a2d851615f8f5866932
Kaynat Shabbir <shabbirkainat75@gmail.com>
2024-10-18 15:14:12 +0500

    Add files via upload

---
+++ file.js
@@ -0,0 +1,20 @@
+//alert ("WELCOME TO SMIT !!");
+
+//let a = prompt("What is your'e name ?");
+
+//let b =prompt("What is your'e father name ?");
+
+//let c =prompt("What is your'e gender ?");
+
+//let course =prompt("ok let me know in which course you want to enrolled?");
+
+//let campus=prompt("Enter your campus name please ?");
+//if(course =="web and app developemnet " || course =="Graphic designing" || course =="mobile app"||course =="AI" ){
+//if(campus=="numaish"|| campus == "gulshan" ||campus=="pechs" ){
+   // console.log("congrats !!\n you are enrolled in this course!!");
+//}
+//}
+//else{
+//alert("sorry !! you are not selescted for this course");
+//console.log("sorry!! you are not selected for this course");
+//}
